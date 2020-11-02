0a028e0d90b472313e3a05536af34dca1e29e345
zeevNoiman <zevnoiman@gmail.com>
2020-11-02 13:43:08 +0200

    started teacher form

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,23 +8,24 @@ import backIcon from '../../assets/images/icons/back.svg'
 
 interface PageHeaderProps{
     title: string;
+    description?: string;
 };
 
 const PageHeader: React.FC <PageHeaderProps> = (props) => {
     return(
         <header className="page-header">
-        <div className="top-bar-container">
-          <Link to='/'>
-            <img src={backIcon} alt="Back"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
+          <div className="top-bar-container">
+            <Link to='/'>
+              <img src={backIcon} alt="Back"/>
+            </Link>
+            <img src={logoImg} alt="Proffy"/>
+          </div>
 
-        <div className="header-content">
-            <strong>{props.title}</strong>
-
+          <div className="header-content">
+              <strong>{props.title}</strong>
+              { props.description && <p>{props.description}</p> }
             {props.children}
-        </div>
+          </div>
       </header>
     )
 }
--- web/src/pages/TeachersForm/index.tsx
+++ web/src/pages/TeachersForm/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-// import { Container } from './styles';
+import './styles.css';
 
 function TeachersForm(){
-  return <PageHeader title='testando' />;
+  return (
+    <div id='page-teacher-form' className='container'>
+      <PageHeader title='Great! You want to give classes!'
+                  description='The first step is to fullfill this form' />
+      <main>
+        <fieldset>
+          <legend>Your info</legend>
+          
+          <div className="input-block">
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name'/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id='avatar'/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id='whatsapp'/>
+          </div>
+        </fieldset>
+      </main>
+    </div>
+  )
 }
 
 export default TeachersForm;
