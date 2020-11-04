39c6e2389056e49db2ff97009bc795b416005728
zeevNoiman <zevnoiman@gmail.com>
2020-11-04 22:49:46 +0200

    listing teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,39 +4,54 @@ import './style.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+export interface Teacher{
+    avatar: string,
+    bio: string,
+    whatsapp: string,
+    user_id:number,
+    subject: string,
+    name: string, 
+    cost: number
+}
 
-export default function TeacherItem(){
+interface TeacherItemProps{
+  teacher: Teacher
+}
+
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher})=>{
     return(
         
 <article className='teacher-item'>
 <header>
-  <img src="https://avatars3.githubusercontent.com/u/29253592?s=400&u=44a8f8c5017aae60ef4e7dcf41d6a0886a04665a&v=4" alt="avatar"/>
+  <img src={teacher.avatar} alt="avatar"/>
   <div>
     <strong>
-      Daniel Najman
+      {teacher.name}
     </strong>
     <span>
-      Quimica
+      {teacher.subject}
     </span>
   </div>
 </header>
 
 <p>
-  Enstusiasta das meljores tecnologias de quimica avancada
-  <br/><br/>
-  Apaixonado por ajudar outras pessoas, dar aulas e dar vida a sua aulas atraves de novas experiencias.
+  {teacher.bio}
 </p>
 
 <footer>
   <p>
     Preco por hora
-    <strong>20 shekel</strong>
+    <strong>{teacher.cost}</strong>
   </p>
-    <button type='button'>
+    <a href={`https://wa.me/${teacher.whatsapp}`}>
       <img src={whatsappIcon} alt="whatsapp"/>
       Enter in touch
-    </button>
+    </a>
 </footer>
 </article>
     )
 }
+
+export default TeacherItem
--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
 import Select from '../../components/Select';
 import './styles.css';
+import api from '../../services/api';
+
+
 
 function TeachersList(){
+
+  const [teachers, setTeachers] = useState([])
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers(ev: FormEvent){
+    ev.preventDefault();
+    console.log(subject, week_day, time);
+    
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    })
+
+    setTeachers(response.data)
+
+  };
+
   return (
     <div id="page-teacher-list">
       <PageHeader title='Those are the disponible teachers'>
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
           <Select options={[
              {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
-             {value: 'Art', label: 'Art'},
+             {value: 'Cauculus', label: 'Cauculus'},
+             {value: 'Computer Arquitecture', label: 'Computer Arquitecture'},
+             {value: 'History', label: 'History'},
+             {value: 'Data Structure', label: 'Data Structure'},
+             {value: 'Algorithms', label: 'Algorithms'},
+             {value: 'Compilers', label: 'Compilers'},
            ]}
+           onChange={ev => {setSubject(ev.target.value)}}
+           value={subject}
           name='subject' 
           label='Subject'></Select>
          <Select options={[
@@ -30,19 +57,23 @@ function TeachersList(){
              {value: '5', label: 'Friday'},
              {value: '6', label: 'Saturday'},
            ]}
+           onChange={ev => {setWeekDay(ev.target.value)}}
+           value={week_day}
           name='week-day' 
           label='Week day'></Select>
-         <Input type='time' name='time' label='Time' ></Input>
+         <Input onChange={ev => {setTime(ev.target.value)}}
+           value={time} type='time' name='time' label='Time' ></Input>
          
+         <button type='submit'>
+           Find
+         </button>
         </form>
       </PageHeader>  
 
       <main>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.user_id} teacher={teacher }/>
+        })}
       </main>
     </div>
   );
